3cd52353b8ba45660f058998ff970ec926f0e8ef
Hadi <hadi.deris.h82@gmail.com>
2024-07-02 23:28:58 +0330

    create-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home', 
+    loadChildren: () => import('./moduls/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'panel', 
+    loadChildren: () => import('./moduls/panel/panel.module').then(m => m.PanelModule)
+  },
+  {
+    path: 'user', 
+    loadChildren: () => import('./moduls/user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+//ng g module home --routing//
+//ng g c home //
